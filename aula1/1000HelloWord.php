e285a062a26934055f02128350ef7e1c925f35bc
Mariaaaaaahn <pontinm758@gmail.com>
2023-03-07 10:19:16 -0300

    oi

--- aula1/1000HelloWord.php
+++ aula1/1000HelloWord.php
@@ -1,5 +1,11 @@
 <?php
 
-echo "Hello World!\n";
+$a = intval(fgets(STDIN));
+$b = intval(fgets(STDIN));
+$c = intval(fgets(STDIN));
+$d = intval(fgets(STDIN));
 
+$is_accepted = boolval($b > $c) & boolval($d > $a) & boolval(($c + $d) > ($a + $b)) & boolval($c > 0) & boolval($d > 0) & boolval($a % 2 == 0);
+
+echo $is_accepted ? "Valores aceitos\n" : "Valores nao aceitos\n";
 ?>
