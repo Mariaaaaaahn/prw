fa9dd09ee347dfc962986b0510aab69a89f51fc6
Mariaaaaaahn <pontinm758@gmail.com>
2023-04-18 11:28:59 -0300

    inferno

---
+++ formulario/ex3.php
@@ -0,0 +1,101 @@
+<?php
+$estado =$_POST['estado'];
+
+//$estado = strtouplower($_POST["estado"]);
+	if ($estado == "SP") {
+          echo "A capital do estado é São Paulo";
+
+	} else if($estado == "AC"){
+				echo "<h3>Rio Branco</h3>";
+		}
+			else if($estado == "AL"){
+				echo "<h3>Maceió</h3>";
+			}
+			else if($estado == "AP"){
+				echo "<h3>Macapá</h3>";
+			}
+			else if($estado == "AM"){
+				echo "<h3>Manaus</h3>";
+			}
+			else if($estado == "BA"){
+				echo "<h3>Salvador</h3>";
+			}
+			else if($estado == "CE"){
+				echo "<h3>Fortaleza</h3>";
+			}
+			else if($estado == "DF"){
+				echo "<h3>Brasília</h3>";
+			}
+			else if($estado == "ES"){
+				echo "<h3>Vitória</h3>";
+			}
+			else if($estado == "GO"){
+				echo "<h3>Goiania</h3>";
+			}
+			else if($estado == "MA"){
+				echo "<h3>São LUIS</h3>";
+			}
+			else if($estado == "MT"){
+				echo "<h3>CUIABA</h3>";
+			}
+			else if($estado == "MS"){
+				echo "<h3>Campo grande</h3>";
+			}
+			else if($estado == "MG"){
+				echo "<h3>Belo Horizonte</h3>";
+			}
+			else if($estado == "PA"){
+				echo "<h3>Belem</h3>";
+			}
+			else if($estado == "PB"){
+				echo "<h3>jOAO DA MATA</h3>";
+			}
+			else if($estado == "PR"){
+				echo "<h3>Curitiba</h3>";
+			}
+			else if($estado == "PE"){
+				echo "<h3>Recife</h3>";
+			}
+			else if($estado == "PI"){
+				echo "<h3>Teresina</h3>";
+			}
+			else if($estado == "RJ"){
+				echo "<h3>Rio de Janeiro</h3>";
+			}
+			else if($estado == "RN"){
+				echo "<h3>Natal</h3>";
+			}else if($estado == "RS"){
+				echo "<h3>Porto Alegre</h3>";
+			}
+			else if($estado == "RO"){
+				echo "<h3>Porto Velho</h3>";
+			}
+			else if($estado == "RR"){
+				echo "<h3>Boa Vista</h3>";
+			}
+			else if($estado == "SC"){
+				echo "<h3>Florianopolis</h3>";
+			}
+			else if($estado == "SE"){
+				echo "<h3>aRACAJU</h3>";
+			}
+			else if($estado == "TO"){
+				echo "<h3>Palmas</h3>";
+			}
+
+
+
+
+
+
+			else
+			echo "Estado invalido";
+
+				
+			
+		
+	?>
+
+ 
+
+
